589c0ea142ec683dd5b2bcbc40b46c0fb69d11a6
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-05 15:28:12 -0600

    Major Game Flow implemented
    -Added an array of passwords for each difficulty level
    -Added a new Screen type of Intro
    -Created GetPlayerName fuction
    -Quite a bit of code refactoring and cleanup
    -Added new main menu for 007 easter egg
    -created a system for randomly selecting a password for each level
    -added the password anagram hint
    -added level name to each puzzle
    -created reward screens for each level

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -1,35 +1,63 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
-    //gamestate
+    // Game configuration data
+    string[] level1Passwords = { "jail", "booked", "keys", "police", "pistol" };
+    string[] level2Passwords = { "agent", "federal", "wanted", "bureau", "investigation" };
+    string[] level3Passwords = { "department", "defense", "general", "terrorist", "military" };
+
+    // Game State
     int level;
-    enum Screen { MainMenu, Password, Win };
+    enum Screen { MainMenu, Password, Win, Intro };
     Screen currentScreen;
     string password;
+    string levelName;
+    string playerName;
 
     // Use this for initialization
     void Start()
     {
-        ShowMainMenu();
+        GetPlayerName();
     }
 
-    void ShowMainMenu()
+
+    void GetPlayerName()
     {
         Terminal.ClearScreen();
-        currentScreen = Screen.MainMenu;
-        Terminal.WriteLine("Welcome, to the Hackertron 2000");
-        Terminal.WriteLine(" ");
-        Terminal.WriteLine("Please select the facility in which you wish to hack.");
-        Terminal.WriteLine(" ");
-        Terminal.WriteLine("1. Local Police Station");
-        Terminal.WriteLine("2. FBI Field Office - Springfield, IL");
-        Terminal.WriteLine("3. The Pentagon - Washington, D.C.");
-        Terminal.WriteLine(" ");
-        Terminal.WriteLine("Enter Selection:");
+        currentScreen = Screen.Intro;
+        Terminal.WriteLine("Please enter username to begin");
+    }
+    void ShowMainMenu(string playerName)
+    {
+        if (playerName == "007")
+        {
+            Terminal.ClearScreen();
+            currentScreen = Screen.MainMenu;
+            Terminal.WriteLine("Welcome Mr. Bond to the MI6 Terminal");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("Please select the facility in which you wish to hack.");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("1. Local Police Station");
+            Terminal.WriteLine("2. FBI Field Office - Springfield, IL");
+            Terminal.WriteLine("3. The Pentagon - Washington, D.C.");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("Enter Selection:");
+        }
+        else
+        {
+            Terminal.ClearScreen();
+            currentScreen = Screen.MainMenu;
+            Terminal.WriteLine("Welcome " + playerName + " to the Hacker-tron 2000");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("Please select the facility in which you wish to hack.");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("1. Local Police Station");
+            Terminal.WriteLine("2. FBI Field Office - Springfield, IL");
+            Terminal.WriteLine("3. The Pentagon - Washington, D.C.");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("Enter Selection:");
+        }
     }
 
     void OnUserInput(string userInput)
@@ -38,7 +66,7 @@ public class Hacker : MonoBehaviour
 
         if (userInput == "menu" || userInput == "Menu") // we can always go directly to main menu
         {
-            ShowMainMenu();
+            ShowMainMenu(playerName);
         }
         else if (currentScreen == Screen.MainMenu)
         {
@@ -46,30 +74,23 @@ public class Hacker : MonoBehaviour
         }
         else if (currentScreen == Screen.Password)
         {
-            CorrectPassword(userInput);
+            CheckPassword(userInput);
+        }
+        else if (currentScreen == Screen.Intro)
+        {
+            playerName = userInput;
+            ShowMainMenu(playerName);
         }
 
     }
 
     void RunMainMenu(string userInput)
     {
-        if (userInput == "1")
+        bool isValidLevelNumber = (userInput == "1" || userInput == "2" || userInput == "3");
+        if (isValidLevelNumber)
         {
-            level = 1;
-            StartGame();
-        }
-        else if (userInput == "2")
-        {
-            level = 2;
-            StartGame();
-        }
-        else if (userInput == "007")
-        {
-            Terminal.WriteLine("Welcome Agent Bond.");
-        }
-        else if (userInput == "poop" || userInput == "shit" || userInput == "pussy" || userInput == "crap")
-        {
-            Terminal.WriteLine("Please be mature when using the Hacker-tron 2000");
+            level = int.Parse(userInput);
+            AskForPassword();
         }
         else
         {
@@ -77,34 +98,110 @@ public class Hacker : MonoBehaviour
         }
     }
 
-    void StartGame()
+    void AskForPassword()
     {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password.");
-        if (level == 1)
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Please enter the password to access " + levelName);
+        Terminal.WriteLine("hint: " + password.Anagram());
+        Terminal.WriteLine("");
+
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
         {
-            password = "Jail";
-        }
-        else if (level == 2)
-        {
-            password = "Agent";
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                levelName = "Local Police Station";
+                break;
+
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                levelName = "FBI Field Office";
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                levelName = "The Pentagon";
+                break;
+            default:
+                Debug.LogError("Invalid Level Number");
+                break;
         }
     }
 
-    private void CorrectPassword(string userInput)
+    void CheckPassword(string userInput)
     {
-        if (level == 1 && userInput == password)
+        if (userInput == password)
         {
-            Terminal.WriteLine("Well Done!");
-        }
-        else if (level == 2 && userInput == password)
-        {
-            Terminal.WriteLine("Well Done!");
+            DisplayWinScreeen();
         }
         else
         {
-            Terminal.WriteLine("try again!");
+            Terminal.WriteLine("Wrong password, try again!");
+            Terminal.WriteLine("hint: " + password.Anagram());
         }
+    }
+
+    void DisplayWinScreeen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("You're in!");
+                Terminal.WriteLine("Here are the local criminal records");
+                Terminal.WriteLine(@"
+ 
+    _________
+   /        /
+  / Crime  / 
+ / Record /        
+/________/                
+"
+                );
+                break;
+            case 2:
+                Terminal.WriteLine("You're in!");
+                Terminal.WriteLine("Here is the FBI's Most Wanted list");
+                Terminal.WriteLine(@"
+      _________  
+     /        //    
+    /  FBI   //     
+   /  Most  //      
+  / Wanted //
+ /________//
+(________(/                
+"
+                );
+                break;
+            case 3:
+                Terminal.WriteLine("You're in!");
+                Terminal.WriteLine("Here is the Terrorist Manifesto");
+                Terminal.WriteLine(@"
+       ____________
+     //          //
+    // Pentagon //  
+   //Terrorist //    
+  //  Guide   //    
+ //_________ //     
+((__________(/               
+"
+                );
+
+                break;
+            default:
+                Debug.LogError("Level is invalid");
+                break;
+        }
+
     }
 }
